cb09dfdfa2875d69231678ad91dacb35ca89aa83
Usova Darya <usovadasha@myttk.ru>
2021-03-01 01:04:19 +0700

    Update CCell.cpp

--- CCell.cpp
+++ CCell.cpp
@@ -6,21 +6,12 @@
 #include "DataOperations.h"
 
 
-//std::vector<float> &cof_corr
 CCell::CCell() {
-  //  this->cof_corr = cof_corr;
 }
 
-//float CCell::calk_result(std::vector<std::vector<float>> &planes_part, std::vector<std::vector<float>> &d) {
 float CCell::calk_result(std::vector<float> &planes_part, std::vector<float> &d) {
     float res = 0.0f;
 
-    /*
-    for(int plane = 0; plane < planes_part.size(); plane++) {
-        res += DataOperations::scalar_multiply(planes_part[plane], d[plane]);
-       // res *= cof_corr[plane];
-    }
-     */
     res += DataOperations::scalar_multiply(planes_part, d);
 
     if(res <= 0.0)
