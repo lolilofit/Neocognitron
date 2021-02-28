dccc1461acd6d405601eb3917160223d9e9a9604
Usova Darya <usovadasha@myttk.ru>
2021-03-01 00:58:46 +0700

    Update ImageProcess.cpp

--- ImageProcess.cpp
+++ ImageProcess.cpp
@@ -44,47 +44,22 @@ void ImageProcess::readImage(int NumberOfImages, int DataOfAnImage, std::vector<
         file.read((char *) &n_cols, sizeof(n_cols));
         n_cols = reverseInt(n_cols);
 
-        /*
-        arr.reserve(number_of_images);
-        for(int i = 0; i < number_of_images; i++) {
-            arr[i].reserve(n_rows*n_cols);
-        }
-         */
-
         data = new char[number_of_images*n_cols*n_rows];
         file.read(data, number_of_images*n_cols*n_rows);
 
         for(int j = 0; j < number_of_images; j++) {
             for(int k = 0; k < n_rows; k++) {
                 for(int l = 0; l < n_cols; l++) {
-        //            std::cout << data[j*n_cols*n_rows + k*n_rows + l];
                     arr[j][k*n_cols + l] =  (float)data[j*n_cols*n_rows + k*n_cols + l];
                 }
-          //      std::cout << "\n";
-            }
-            //std::cout << "\n\n";
-        }
-        //std::cout << "";
-
-/*
-        for (int i = 0; i < number_of_images; ++i) {
-            for (int r = 0; r < n_rows; ++r) {
-                for (int c = 0; c < n_cols; ++c) {
-                    unsigned char temp = 0;
-                    file.read((char *) &temp, sizeof(temp));
-                    arr[i][(n_cols * r) + c] = (float) temp;
-                }
             }
         }
-         */
-
         file.close();
         delete[](data);
     }
 }
 
 void ImageProcess::read_label(std::vector<int> &arr, std::string filename) {
-   // arr.resize(NumberOfImages, std::vector<float>(DataOfAnImage));
     std::ifstream file(filename);
 
     if (file.is_open()) {
